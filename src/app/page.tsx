405bb13fa1b4bf72b9cd570ff5f022b22d1b40df
harshfrontend <harshlimbachiya1823@gmail.com>
2024-03-23 13:22:45 +0530

    test: commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,18 +27,7 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      Hello world!
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
